1a7d637ea0c0483f460455ac3b4414c01f1789c8
ATSUMI KANETA <42095292+acchi17@users.noreply.github.com>
2018-10-27 17:02:30 +0900

    Update and rename joytostdmsg to joytostdmsg.py

---
+++ joytostdmsg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Joy
+from std_msgs.msg import UInt8MultiArray
+
+btnlist = [0] * 14
+
+def joytostdarray(ds4msg):
+    global btnlist
+    for i in range(14)
+      btnlist[i] = ds4msg.buttons[i]
+
+if __name__ == '__main__':
+  rospy.init_node('joytostdmsg')  
+  rospy.Subscriber("joy", Joy, joytostdarray)
+  pub = rospy.Publisher('ds4btns', UInt8MultiArray, queue_size = 1)
+  
+  rate = rospy.Rate(10)
+  while not rospy.is_shutdown():
+    pub.publish(btnlist)
+    rate.sleep()
